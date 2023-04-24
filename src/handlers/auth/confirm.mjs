da07ca726024aca01a6880712de6610c0049d500
i8lex <cybeerid@gmail.com>
2023-04-24 19:15:32 +0300

    fix  confirm

--- src/handlers/auth/confirm.mjs
+++ src/handlers/auth/confirm.mjs
@@ -1,5 +1,5 @@
 import { User } from "./register.mjs";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 // import { SECRET_WORD } from "../../config/index.mjs";
 import * as jwt from "jsonwebtoken";
 
@@ -8,22 +8,23 @@ dotenv.config();
 const { verify } = jwt.default;
 
 export const confirmEmailHandler = async (request, reply) => {
-
-    const { confirm } = request.query
+  try {
+    const { confirm } = request.query;
     const { email } = await verify(confirm, process.env.SECRET_WORD);
-
-    try {
-        const updateStatus = await User.updateOne({ email:email }, { isconfirmed: true });
-
-        if (updateStatus.nModified === 0) {
-            return reply.status(404).send('User not found');
-        }
-
-    } catch (err) {
-        reply.status(500).send(err);
+    console.log(email);
+    const updateStatus = await User.updateOne(
+      { email: email },
+      { isconfirmed: true }
+    );
+    if (updateStatus.nModified === 0) {
+      return reply.status(404).send("User not found");
+    } else {
+      reply.send({
+        message: "Successfully confirmed",
+        email,
+      });
     }
-        reply.send({
-                message: 'Successfully confirmed', email
-        })
-}
-
+  } catch (error) {
+    reply.status(401).send(error);
+  }
+};
