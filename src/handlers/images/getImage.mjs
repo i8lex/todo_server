16b4735fabca075927e99269575dfa7018e18e10
i8lex <cybeerid@gmail.com>
2023-05-04 22:43:06 +0300

    update image logic

--- src/handlers/images/getImage.mjs
+++ src/handlers/images/getImage.mjs
@@ -25,10 +25,10 @@ export const getImageHandler = async (request, reply) => {
     // console.log(data)                         // upload from server
     // reply.type(image.mimetype).send(data);    //
 
-    const { image: data, mimetype, filename } = image;
-    reply.header("Content-Type", mimetype);
-    reply.header("Content-Disposition", `attachment; filename=${filename}`);
-    reply.send(Buffer.from(data));
+    // const { image: data, mimetype, filename } = image;
+    // reply.header("Content-Type", mimetype);
+    // reply.header("Content-Disposition", `attachment; filename=${filename}`);
+    reply.send(image);
   } catch (err) {
     console.log(err);
     reply.code(500).send({ message: "Server error" });
